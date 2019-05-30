c66b60cf2c00847b22e408613dc5898b85c2e49b
zhijund <762718879@qq.com>
2019-05-30 17:50:39 +0800

    update

--- dunzung-cloud-framework/dunzung-cloud-framework-service/src/main/java/com/dunzung/cloud/framework/service/BaseService.java
+++
@@ -1,7 +0,0 @@
-package com.dunzung.cloud.framework.service;
-
-/**
- * Created by Wooola on 2018/9/10.
- */
-public interface BaseService<T> {
-}
--- dunzung-cloud-framework/dunzung-cloud-framework-service/src/main/java/com/dunzung/cloud/framework/service/impl/BaseServiceImpl.java
+++
@@ -1,7 +0,0 @@
-package com.dunzung.cloud.framework.service.impl;
-
-import com.dunzung.cloud.framework.service.BaseService;
-
-public class BaseServiceImpl implements BaseService {
-
-}
