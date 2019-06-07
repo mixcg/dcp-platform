eca34e8331569fe76dcb07abd8c2ebca698eaa8f
zhijund <762718879@qq.com>
2019-06-07 12:01:40 +0800

    添加security代码

--- dunzung-cloud-framework/dunzung-cloud-framework-dao/src/main/java/com/dunzung/cloud/framework/base/CloudMapper.java
+++ dunzung-cloud-framework/dunzung-cloud-framework-dao/src/main/java/com/dunzung/cloud/framework/base/CloudMapper.java
@@ -10,28 +10,4 @@ import java.util.Map;
  */
 public interface CloudMapper<T> extends BaseMapper<T> {
 
-    void save(T t);
-
-    void save(Map<String, Object> map);
-
-    void saveBatch(List<T> list);
-
-    int update(T t);
-
-    int update(Map<String, Object> map);
-
-    int delete(Object id);
-
-    int delete(Map<String, Object> map);
-
-    int deleteBatch(Object[] id);
-
-    T queryObject(Object id);
-
-    List<T> queryList(Map<String, Object> map);
-
-    int queryTotal(Map<String, Object> map);
-
-    int queryTotal();
-
 }
--- dunzung-cloud-framework/dunzung-cloud-framework-dao/src/main/java/com/dunzung/cloud/framework/base/service/impl/MybatisServiceImpl.java
+++ dunzung-cloud-framework/dunzung-cloud-framework-dao/src/main/java/com/dunzung/cloud/framework/base/service/impl/MybatisServiceImpl.java
@@ -2,8 +2,8 @@ package com.dunzung.cloud.framework.base.service.impl;
 
 import com.baomidou.mybatisplus.service.impl.ServiceImpl;
 import com.dunzung.cloud.framework.base.service.MybatisService;
-import com.dunzung.cloud.framework.base.PortalMapper;
+import com.dunzung.cloud.framework.base.CloudMapper;
 
-public class MybatisServiceImpl<M extends PortalMapper<T>, T> extends ServiceImpl<M, T> implements MybatisService<T> {
+public class MybatisServiceImpl<M extends CloudMapper<T>, T> extends ServiceImpl<M, T> implements MybatisService<T> {
 
 }
