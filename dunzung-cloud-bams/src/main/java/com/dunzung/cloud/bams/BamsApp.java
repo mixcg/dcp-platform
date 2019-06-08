9941d1afb9cbfb347ea1bce2a14aa9f2058601d8
zhijund <762718879@qq.com>
2019-06-08 21:08:01 +0800

    添加安全框架代码

--- dunzung-cloud-bams/src/main/java/com/dunzung/cloud/bams/BamsApp.java
+++ dunzung-cloud-bams/src/main/java/com/dunzung/cloud/bams/BamsApp.java
@@ -4,10 +4,10 @@ import com.dunzung.cloud.framework.mvc.ApplicationStarter;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BamsApplication {
+public class BamsApp {
 
     public static void main(String[] args) {
-        ApplicationStarter.run(BamsApplication.class, args);
+        ApplicationStarter.run(BamsApp.class, args);
     }
 
 }
