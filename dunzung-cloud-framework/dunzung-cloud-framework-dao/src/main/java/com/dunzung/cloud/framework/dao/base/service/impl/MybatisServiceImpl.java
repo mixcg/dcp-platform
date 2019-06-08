c4d3bb4ca68483464d5188e7e8d3a6118f868651
zhijund <762718879@qq.com>
2019-06-08 20:22:40 +0800

    提交uas代码

--- dunzung-cloud-framework/dunzung-cloud-framework-dao/src/main/java/com/dunzung/cloud/framework/dao/base/service/impl/MybatisServiceImpl.java
+++ dunzung-cloud-framework/dunzung-cloud-framework-dao/src/main/java/com/dunzung/cloud/framework/dao/base/service/impl/MybatisServiceImpl.java
@@ -1,9 +1,14 @@
 package com.dunzung.cloud.framework.dao.base.service.impl;
 
 import com.baomidou.mybatisplus.service.impl.ServiceImpl;
-import com.dunzung.cloud.framework.dao.base.service.MybatisService;
 import com.dunzung.cloud.framework.dao.base.CloudMapper;
+import com.dunzung.cloud.framework.dao.base.service.MybatisService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class MybatisServiceImpl<M extends CloudMapper<T>, T> extends ServiceImpl<M, T> implements MybatisService<T> {
-
+    /**
+     * 日志对象
+     */
+    protected Logger logger = LoggerFactory.getLogger(getClass());
 }
