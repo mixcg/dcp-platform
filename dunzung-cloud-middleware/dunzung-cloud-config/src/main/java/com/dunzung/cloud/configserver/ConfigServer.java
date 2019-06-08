1d058e35158273fe322e5ef648d392c6c9dce3b0
zhijund <762718879@qq.com>
2019-06-08 17:46:10 +0800

    提交sso代码

--- dunzung-cloud-middleware/dunzung-cloud-config/src/main/java/com/dunzung/cloud/configserver/ConfigServer.java
+++ dunzung-cloud-middleware/dunzung-cloud-config/src/main/java/com/dunzung/cloud/configserver/ConfigServer.java
@@ -1,4 +1,4 @@
-package com.dunzung.cloud.middleware;
+package com.dunzung.cloud.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
